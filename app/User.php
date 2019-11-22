41c5a653dc9ff273dcd102c6daa7c042bf14811b
solembum-php <solembum@sole.com>
2019-11-22 20:21:24 +0200

    add to Model User

--- app/User.php
+++ app/User.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use App\Models\Service;
 class User extends Authenticatable
 {
     use Notifiable;
@@ -26,4 +26,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function services(){
+	return $this->hasMany(Service::class);
+    }
 }
