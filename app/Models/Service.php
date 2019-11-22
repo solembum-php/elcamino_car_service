650bc04ce814edae0d4f87a65817ce84332c55ba
solembum-php <solembum@sole.com>
2019-11-22 20:20:59 +0200

    create Model Service

---
+++ app/Models/Service.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+
+class Service extends Model
+{
+    /**
+     *
+     * @var array
+     */
+    protected $fillable = [
+	'name', 'id',
+    ];
+    public function user(){
+    return $this->belongsTo(User::class);
+    }
+}
