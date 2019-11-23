6b072da8bef75aa213357b3d677df7276d12cb71
solembum-php <solembum@i.ua>
2019-11-24 00:03:48 +0200

    create seeders

---
+++ database/seeds/ServicesTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ServicesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         DB::table('services')->insert([
+	    'name' => 'painting',
+	    'user_id' => 1,
+	]);
+	DB::table('services')->insert([
+	    'name' => 'straightening',
+	    'user_id' => 1,
+	]);
+	DB::table('services')->insert([
+	    'name' => 'body parts replacement',
+	    'user_id' => 2,
+	]);
+	DB::table('services')->insert([
+	    'name' => 'car air conditioner repair',
+	    'user_id' => 2,
+	]);
+	DB::table('services')->insert([
+	    'name' => 'car air conditioner installation',
+	    'user_id' => 3,
+	]);
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+	    'name' => 'qwe',
+	    'email' => 'qwe@gmail.com',
+	    'password' => bcrypt('123123'),
+	]);
+	DB::table('users')->insert([
+	    'name' => 'asd',
+	    'email' => 'asd@gmail.com',
+	    'password' => bcrypt('123123'),
+	]);
+	DB::table('users')->insert([
+	    'name' => 'zxc',
+	    'email' => 'zxc@gmail.com',
+	    'password' => bcrypt('123123'),
+	]);
+    }
+}
