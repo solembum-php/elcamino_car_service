2ce2e6526d9fc213e1854a6530cab5ce0446851d
solembum-php <solembum@i.ua>
2019-11-24 00:02:33 +0200

    create Auth and Pricelist Controllers

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Service;
+class AuthController extends Controller
+{
+    protected $redirectTo = '/home';
+}
