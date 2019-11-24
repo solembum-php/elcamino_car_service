55e7326a1959c2ad20766895318cbe4cf4144fda
solembum-php <solembum@i.ua>
2019-11-24 21:19:35 +0200

    ServicePolicy.php and ValidateDestroy.php

---
+++ app/Policies/ServicePolicy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\User;
+use App\Models\Service;
+
+class ServicePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+//    public function __construct()
+//    {
+//        //
+//    }
+    /**
+     * 
+     * @param User $user
+     * @param Service $service
+     * @return type
+     */
+    public function destroy(User $user, Service $service) {
+	return $service->user()->first()->id == $user->id;
+    }
+}
